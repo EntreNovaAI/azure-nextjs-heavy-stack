5a5f0bafe3c72c34e27a93a2025afbceb836d925
Brandon Melville <melvillebrandon7@gmail.com>
2025-09-10 21:34:33 -0500

    add home page

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Template',
+  description: 'Login',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
